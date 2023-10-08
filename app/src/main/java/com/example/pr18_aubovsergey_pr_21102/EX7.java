be5affb722f1c42247efe62b49ae2a43cbe032fa
Sergey <sergeypanz355@mail.ru>
2023-10-08 22:30:36 +0700

    Задание 7 готово

--- app/src/main/java/com/example/pr18_aubovsergey_pr_21102/EX7.java
+++ app/src/main/java/com/example/pr18_aubovsergey_pr_21102/EX7.java
@@ -2,13 +2,82 @@ package com.example.pr18_aubovsergey_pr_21102;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.SimpleCursorTreeAdapter;
 
 public class EX7 extends AppCompatActivity {
+
+    ExpandableListView elvMain;
+    DBEX7 db;
+    Button btnNext, btnPrev;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ex7);
+
+        btnNext = findViewById(R.id.btnNext);
+        btnPrev = findViewById(R.id.btnPrev);
+        btnNext.setOnClickListener(v -> {next();});
+        btnPrev.setOnClickListener(v -> {prev();});
+
+// подключаемся к БД
+        db = new DBEX7(this);
+        db.open();
+
+        // готовим данные по группам для адаптера
+        Cursor cursor = db.getCompanyData();
+        startManagingCursor(cursor);
+        // сопоставление данных и View для групп
+        String[] groupFrom = { DBEX7.COMPANY_COLUMN_NAME };
+        int[] groupTo = { android.R.id.text1 };
+        // сопоставление данных и View для элементов
+        String[] childFrom = { DBEX7.PHONE_COLUMN_NAME };
+        int[] childTo = { android.R.id.text1 };
+
+        // создаем адаптер и настраиваем список
+        SimpleCursorTreeAdapter sctAdapter = new MyAdapter(this, cursor,
+                android.R.layout.simple_expandable_list_item_1, groupFrom,
+                groupTo, android.R.layout.simple_list_item_1, childFrom,
+                childTo);
+        elvMain = (ExpandableListView) findViewById(R.id.elvMain);
+        elvMain.setAdapter(sctAdapter);
+    }
+
+    public void next(){
+        Intent intent = new Intent(this, EX8.class);
+        startActivity(intent);
+    }
+
+    public void prev(){
+        Intent intent = new Intent(this, EX6.class);
+        startActivity(intent);
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        db.close();
+    }
+
+    class MyAdapter extends SimpleCursorTreeAdapter {
+
+        public MyAdapter(Context context, Cursor cursor, int groupLayout,
+                         String[] groupFrom, int[] groupTo, int childLayout,
+                         String[] childFrom, int[] childTo) {
+            super(context, cursor, groupLayout, groupFrom, groupTo,
+                    childLayout, childFrom, childTo);
+        }
+
+        protected Cursor getChildrenCursor(Cursor groupCursor) {
+            // получаем курсор по элементам для конкретной группы
+            int idColumn = groupCursor.getColumnIndex(DBEX7.COMPANY_COLUMN_ID);
+            return db.getPhoneData(groupCursor.getInt(idColumn));
+        }
     }
 }
