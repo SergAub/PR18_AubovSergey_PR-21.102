de2768aeb410f5f701fa71f48f6d31ed356a914a
Sergey <sergeypanz355@mail.ru>
2023-10-08 21:33:07 +0700

    Задание 4 готово

---
+++ app/src/main/java/com/example/pr18_aubovsergey_pr_21102/EX5.java
@@ -0,0 +1,14 @@
+package com.example.pr18_aubovsergey_pr_21102;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EX5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex5);
+    }
+}
